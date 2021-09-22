00862a5c2e9972eb464c5efca05a1e12096fdb98
Xavier Arias <xavier_rate@outlook.com>
2021-09-22 21:46:56 +1000

    Starting the actual app itself
    Adding Mockpassword hider and other text based scripting fun!

---
+++ Assets/Gyro your way to love!/Xavier Scripts/InputTextShower.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Gyro_your_way_to_love_.Xavier_Scripts
+{
+    public class InputTextShower : MonoBehaviour
+    {
+        public string theName;
+        public GameObject inputField;
+        public GameObject textDisplay;
+        
+        public void StoreName()
+        {
+            theName = inputField.GetComponent<TextMeshProUGUI>().text;
+            textDisplay.GetComponent<TextMeshProUGUI>().text = theName;
+        }
+        
+    }
+}
