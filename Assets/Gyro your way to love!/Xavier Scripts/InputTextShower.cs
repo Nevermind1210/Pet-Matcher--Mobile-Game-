74d2649af4e1b28dcee56897c1f2b4d04f2e01a2
Xavier Arias <xavier_rate@outlook.com>
2021-09-27 01:47:33 +1000

    Changing some UI functionality and attempting password masking (testing)

--- Assets/Gyro your way to love!/Xavier Scripts/InputTextShower.cs
+++ Assets/Gyro your way to love!/Xavier Scripts/InputTextShower.cs
@@ -1,19 +1,39 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace Gyro_your_way_to_love_.Xavier_Scripts
 {
     public class InputTextShower : MonoBehaviour
     {
-        public string theName;
-        public GameObject inputField;
-        public GameObject textDisplay;
-        
+        [SerializeField] private string theName;
+        [SerializeField] private TMP_InputField inputField;
+        [SerializeField] private GameObject textDisplay;
+        [SerializeField] private TMP_InputField passwordBox;
+
+        private void Update()
+        {
+            if (inputField)
+            {
+                TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, true);
+            }
+            inputField.keyboardType = TouchScreenKeyboardType.Default;
+        }
+
         public void StoreName()
         {
             theName = inputField.GetComponent<TextMeshProUGUI>().text;
             textDisplay.GetComponent<TextMeshProUGUI>().text = theName;
         }
-        
+
+        public void PasswordMask()
+        {
+            if (passwordBox)
+            {
+                TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, true);
+            }
+            passwordBox.keyboardType = TouchScreenKeyboardType.Default;
+            passwordBox.inputType = TMP_InputField.InputType.Password;
+        }
     }
 }
